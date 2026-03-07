2cb611867785465b54d5a4e15ace0be4afef405e
camilopaez900-art <camilopaez900@gmail.com>
2026-03-06 21:12:40 -0500

    creacion puntaje.py

---
+++ semana3/creacion puntaje.py
@@ -0,0 +1,13 @@
+#ALGARITMO
+#1. pedir el primer puntaje 
+#2. pedir segundo puntaje 
+#3. comparar puntaje 1 con puntaje 2
+
+
+# PSEUDOCADIGO
+#   INICIO
+
+
+puntaje1 = int(input("ingrese el primer numero:"))
+puntaje2 = int(input("ingrese el segundo numero:"))
+print( puntaje1 > puntaje2)
