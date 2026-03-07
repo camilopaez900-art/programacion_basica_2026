f880214cdbee0a26497194ef6dda80296c3f03c5
camilopaez900-art <camilopaez900@gmail.com>
2026-03-06 20:57:09 -0500

    Mi primer programa

---
+++ semana3/ejer1.py
@@ -0,0 +1,16 @@
+#esto es un tipo de dato float
+a=3
+#esto ees un tipo de dato entero
+b=300
+print("La suma es:", a+b)
+print("la resta es:",a-b)
+print(" la multiplicacion:",a*b)
+print("la division es:",a/b)
+print("la division entra es:",a//b)
+#el signo porcentaje nos da el residuo de la division
+print("el residuo es:",a%b)
+
+# comparaciones 
+print(a<b)
+print(a>b)
+print(a==b)
