ff2110658c0a5baa5b2e2cdd20f5d4eaa299b486
camilopaez900-art <camilopaez900@gmail.com>
2026-03-13 21:31:27 -0500

    Create Pedir Numero.py

---
+++ semana5/Pedir Numero.py
@@ -0,0 +1,8 @@
+#PROGRAMACION EVALUA MAS DE UNA CONDICION
+NUMERO=float(input("Dame un numero"))
+if NUMERO >0:
+    print("Es un numero positivo")
+elif NUMERO <0:
+    print("Es un numero negativo")
+elif NUMERO==0:
+    print("Es cero")
